0c43d85b43bcd516ea2922858dde807061c69481
Liukaixin <liukaixin@speechocean.com>
2024-02-08 18:28:38 +0800

    update c3

--- Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
+++ Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
@@ -286,12 +286,12 @@ int main(){
 			// TODO: (Filter scan using voxel filter)
             pcl::VoxelGrid<PointT> vg;
             vg.setInputCloud(scanCloud);
-            double filterRes = 0.5;
+            double filterRes = 1.5;
             vg.setLeafSize(filterRes, filterRes, filterRes);
             vg.filter(*cloudFiltered);
 			// TODO: Find pose transform by using ICP or NDT matching
 //			Eigen::Matrix4d eigen_transform = NDT(ndt, cloudFiltered, pose, 300);
-            Eigen::Matrix4d eigen_transform = ICP(mapCloud, cloudFiltered, truePose, 3);
+            Eigen::Matrix4d eigen_transform = ICP(mapCloud, cloudFiltered, truePose, 5);
 			pose = getPose(eigen_transform);
 
 			// TODO: Transform scan so it aligns with ego's actual pose and render that scan
