2ca187bf159e29abb0f0c621e368fcb682c8f689
Liukaixin <liukaixin@speechocean.com>
2024-02-08 17:54:12 +0800

    update c3

--- Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
+++ Lesson_7_Project_Scan_Matching_Localization/c3-project/c3-main.cpp
@@ -286,7 +286,7 @@ int main(){
 			// TODO: (Filter scan using voxel filter)
             pcl::VoxelGrid<PointT> vg;
             vg.setInputCloud(scanCloud);
-            double filterRes = 100;
+            double filterRes = 200;
             vg.setLeafSize(filterRes, filterRes, filterRes);
             vg.filter(*cloudFiltered);
 			// TODO: Find pose transform by using ICP or NDT matching
